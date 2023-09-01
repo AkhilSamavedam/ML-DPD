32dcd3e0f267da0bf19132567a34f50049de391d
Akhil Samavedam <akhil.samavedam@gmail.com>
2023-08-31 20:38:04 -0400

    Added Empty Files to Prevent Path issues

--- model/test.py
+++ model/test.py
@@ -1,8 +1,10 @@
 import numpy as np
 import glob
+import tensorflow as tf
 
 ls = [np.load(fn) for fn in glob.glob('')]
 
 combined_tensor = np.array(ls)
 
-print(combined_tensor.shape)
+print(tf.config.list_physical_devices('GPU'))
+
