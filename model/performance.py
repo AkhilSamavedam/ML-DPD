be9436357ba9a9acb0f4d0b6f3b68036d3fd37c6
Akhil Samavedam <akhil@anils-mbp-2.lan>
2023-09-06 02:33:57 -0500

    Refactored and Upgraded test_edmd into performance

--- model/performance.py
+++ model/performance.py
@@ -4,48 +4,75 @@ import os
 import re
 import pandas as pd
 from config import j
-
+import time
 
 K = np.load(j('results/256/koopman.npy'))
 decoder = tf.keras.models.load_model(j('results/256/decoder.h5'))
 
-
 pathname = j('latent/256')
+state_pathname = j('Numpygits')
 def natural_sort_key(s):
     return [int(text) if text.isdigit() else text for text in re.split(r'(\d+)', s)]
 
 ls = os.listdir(pathname)
+state_ls = os.listdir(state_pathname)
+
+ls = [i for i in ls if i.endswith('.npy')]
+state_ls = [i for i in state_ls if i.endswith('.npy')]
+
 ls.sort(key=natural_sort_key)
+state_ls.sort(key=natural_sort_key)
+
+latent_vectors = np.array([np.load(i) for i in ls])
+states = np.array([np.load(i) for i in state_ls])
+
 
 history_file = j('results/256/edmd.csv')
 
 entries = []
 
+
 for i in range(len(ls) - 10):
 
-    dictionary = {'timestep': int(ls[i][:-4]), 'timestep_predicted': int(ls[i + 1][:-4])}
+    dictionary = {'timestep': int(ls[i][:-4])}
+    latent = latent_vectors[i]
 
     for n in range(1, 10):
-        latent = np.load(os.path.join(pathname, ls[i]))
+
+        start_time = time.time()
         predicted_latent = (np.linalg.matrix_power(K, n) @ latent.T).T
         predicted_state = decoder.predict(predicted_latent)
-        actual_latent = np.load(os.path.join(pathname, ls[i + n]))
-        actual_state = np.load(os.path.join(pathname, j('Numpy'), f'slice_{ls[i + n]}'))
+        end_time = time.time()
+
+        prediction_time = end_time - start_time
+
+        actual_latent = latent_vectors[i + n]
+        actual_state = states[i + n]
 
         latent_mean = tf.reduce_mean(actual_latent)
         state_mean = tf.reduce_mean(actual_state)
 
         latent_rmse = tf.sqrt(tf.reduce_mean(tf.square(actual_latent - predicted_latent)))
         rmse = tf.sqrt(tf.reduce_mean(tf.square(actual_state - predicted_state)))
+
+        latent_mae = tf.reduce_mean(tf.abs(actual_latent - latent))
+        mae = tf.reduce_mean(tf.abs(actual_state - predicted_state))
+
         epsilon = 1e-10
         latent_percent = 100 * np.sum(np.abs(predicted_latent - actual_latent)) / (np.sum(np.abs(actual_latent)) + epsilon)
         percent = 100 * np.sum(np.abs(predicted_state - actual_state)) / (np.sum(np.abs(actual_latent)) + epsilon)
 
-        dictionary[f'latent_rmse${n}'] = float(latent_rmse)
-        dictionary[f'rmse${n}'] = float(rmse)
-        dictionary[f'latent%${n}'] = float(latent_percent)
-        dictionary[f'%${n}'] = float(percent)
+
+        dictionary[f'latent-rmse_{n}'] = float(latent_rmse)
+        dictionary[f'rmse_{n}'] = float(rmse)
+        dictionary[f'latent-mae_{n}'] = float(latent_mae)
+        dictionary[f'mae_{n}'] = float(mae)
+        dictionary[f'latent%_{n}'] = float(latent_percent)
+        dictionary[f'%_{n}'] = float(percent)
+
+        dictionary[f'time_{n}'] = float(prediction_time / 1000)  # converting time to ms
+
+        print(f'{ls[i][:-4]} --> {ls[i + n][:-4]}')
     entries.append(dictionary)
-    print(f'{ls[i][:-4]} --> {ls[i + 1][:-4]}')
 df = pd.DataFrame.from_records(entries, index=['timestep'])
 df.to_csv(j('results/256/edmd.csv'))
