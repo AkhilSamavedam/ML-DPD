1cdbb23a8bd3e0fab22480a214ed0be622a7100a
Akhil Samavedam <akhil.samavedam@gmail.com>
2023-10-13 12:17:02 -0500

    Added Setup.sh for Users wanting to add data directories

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Making Data Directories (These are large and will not be included in the repo
+mkdir CSV
+mkdir Numpy
+mkdir latent
+mkdir Dump
+mkdir MNF
+
+cd latent
+mkdir 128 256 512 1024
