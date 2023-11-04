b5042199cad577140676aebca4934aabef4811d1
Akhil Samavedam <akhil.samavedam@gmail.com>
2023-11-04 13:52:30 -0500

    Moving Around Jax Calls for Custom Layers in Haiku Modules

--- model/autoencoder_jax.py
+++ model/autoencoder_jax.py
@@ -5,7 +5,6 @@ import jax
 import haiku as hk
 from jax import jit
 import jax.numpy as jnp
-from utils import UpSampling2D, Cropping2D
 from config import latent_shape, tensor_shape, j
 from time import time
 import cloudpickle as cpkl
@@ -13,7 +12,48 @@ import orbax
 from glob import glob
 from utils import create_dataset_from_npy_folder
 
+class UpSampling2D(hk.Module):
+    def __init__(self, shape, method='nearest'):
+        super().__init__()
+        self.shape = shape
+        self.method = method
 
+    def __call__(self, x, *args, **kwargs):
+        if x.ndim == 3:  # Detect channel dimension
+            height, width, channels = x.shape
+            new_height = height * self.shape[0]
+            new_width = width * self.shape[1]
+            resized_image = jax.image.resize(x, (new_height, new_width, channels), method=self.method)
+            return resized_image
+        elif x.ndim == 4:  # Detect batch dimension
+            batch_size, height, width, channels = x.shape
+            new_height = height * self.shape[0]
+            new_width = width * self.shape[1]
+            resized_image = jax.image.resize(x, (batch_size, new_height, new_width, channels), method=self.method)
+            return resized_image
+        else:
+            raise ValueError('Invalid input dimension')
+
+
+class Cropping2D(hk.Module):
+    def __init__(self, cropping):
+        super().__init__()
+        self.cropping = cropping
+
+    def __call__(self, x, *args, **kwargs):
+        (top, bottom), (left, right) = self.cropping
+        if x.ndim == 3:
+            if bottom == 0:
+                bottom = -x.shape[0]
+            if right == 0:
+                right = -x.shape[1]
+            return x[top:-bottom, left:-right, :]
+        elif x.ndim == 4:
+            if bottom == 0:
+                bottom = -x.shape[1]
+            if right == 0:
+                right = -x.shape[2]
+            return x[:, top:-bottom, left:-right, :]
 class Encoder(hk.Module):
     def __init__(self, latent_dim, input_shape):
         super().__init__()
--- utils.py
+++ utils.py
@@ -1,7 +1,4 @@
 import tensorflow as tf
-#import jax
-#import jax.numpy as jnp
-#import haiku as hk
 import os
 from config import j, latent_shape, tensor_shape
 import numpy as np
@@ -10,50 +7,6 @@ import re
 def natural_sort_key(s):
     return [int(text) if text.isdigit() else text for text in re.split(r'(\d+)', s)]
 
-'''
-class UpSampling2D(hk.Module):
-    def __init__(self, shape, method='nearest'):
-        super().__init__()
-        self.shape = shape
-        self.method = method
-
-    def __call__(self, x, *args, **kwargs):
-        if x.ndim == 3:  # Detect channel dimension
-            height, width, channels = x.shape
-            new_height = height * self.shape[0]
-            new_width = width * self.shape[1]
-            resized_image = jax.image.resize(x, (new_height, new_width, channels), method=self.method)
-            return resized_image
-        elif x.ndim == 4:  # Detect batch dimension
-            batch_size, height, width, channels = x.shape
-            new_height = height * self.shape[0]
-            new_width = width * self.shape[1]
-            resized_image = jax.image.resize(x, (batch_size, new_height, new_width, channels), method=self.method)
-            return resized_image
-        else:
-            raise ValueError('Invalid input dimension')
-
-
-class Cropping2D(hk.Module):
-    def __init__(self, cropping):
-        super().__init__()
-        self.cropping = cropping
-
-    def __call__(self, x, *args, **kwargs):
-        (top, bottom), (left, right) = self.cropping
-        if x.ndim == 3:
-            if bottom == 0:
-                bottom = -x.shape[0]
-            if right == 0:
-                right = -x.shape[1]
-            return x[top:-bottom, left:-right, :]
-        elif x.ndim == 4:
-            if bottom == 0:
-                bottom = -x.shape[1]
-            if right == 0:
-                right = -x.shape[2]
-            return x[:, top:-bottom, left:-right, :]
-'''
 
 def load_npy_files(file_paths):
     for file_path in file_paths:
